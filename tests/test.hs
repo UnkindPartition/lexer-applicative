ba1988764aa42067b2a7e324400a97db0fefc1cb
Roman Cheplyaka <roma@ro-che.info>
2015-02-21 11:28:34 +0200

    Add a test for #1

--- tests/test.hs
+++ tests/test.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -6,10 +7,15 @@ import Text.Regex.Applicative
 import Text.Regex.Applicative.Common
 import Data.Char
 import Data.Loc (L(..), Loc(..), Pos(..))
+import Control.Exception
+import Control.DeepSeq
 
 whitespace = () <$ some (psym isSpace)
 
 unloc (L l a) = (a, l)
+
+-- this is bad, because it accepts an empty string
+badWhitespace = () <$ many (psym isSpace)
 
 main = defaultMain $ testGroup "Tests"
   [ testCase "Empty string" $
@@ -20,4 +26,18 @@ main = defaultMain $ testGroup "Tests"
       , (23, Loc (Pos "-" 2 1 3) (Pos "-" 2 2 4))
       , (456,Loc (Pos "-" 2 5 7) (Pos "-" 2 7 9))
       ]
+  , testCase "Nullable parser, no error" $ do
+      r <- try . evaluate $ tokens decimal badWhitespace "-" "31 45"
+      case r of
+        Right (_ :: [L Int]) -> return ()
+        Left (e :: SomeException) -> assertFailure $ show e
+  , testCase "Nullable parser, error" $ do
+      r <- try . evaluate . force $ tokens decimal badWhitespace "-" "31? 45"
+      case r of
+        Right (_ :: [L Int]) -> assertFailure "No error?"
+        Left (LexicalError p) -> p @?= Pos "-" 1 2 2
   ]
+
+-- orphan
+instance NFData a => NFData (L a) where
+  rnf (L loc a) = loc `seq` rnf a
